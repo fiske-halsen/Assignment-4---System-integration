f534e9856c8f536c691292551a8d20cae0a66633
LAPTOP-69AO34FQ\lukas <lukasbangstoltz@gmail.com>
2022-11-29 13:03:40 +0100

    feedback first commit

---
+++ WebApplication1/FeedbackService/Context/DBApplicationContext.cs
@@ -0,0 +1,32 @@
+﻿using FeedbackService.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace FeedbackService.Context
+{
+    
+        public class DBApplicationContext : DbContext
+        {
+            public DBApplicationContext(DbContextOptions<DBApplicationContext> options) : base(options) { }
+
+            public DbSet<Review> Reviews { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            Seed(builder);
+            base.OnModelCreating(builder);
+        }
+        private void Seed(ModelBuilder builder)
+        {
+            builder.Entity<Review>().HasData(
+                new Review { UserId = 1, DeliveryId = 1, RestaurantId = 1, Id = 1, Message = "Det var godt nok noget dårlig mad mand!!!", Rating = 1, ReviewDate = DateTime.Now },
+                new Review { UserId = 2, DeliveryId = 1, RestaurantId = 1, Id = 2, Message = "Det var godt nok noget lækker mad mand!!!", Rating = 5, ReviewDate = DateTime.Now }
+
+
+
+                );
+        }
+
+        }
+    }
---
+++ WebApplication1/FeedbackService/Services/ApacheKafkaConsumer.cs
@@ -0,0 +1,64 @@
+﻿using Confluent.Kafka;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace FeedbackService.Services
+{
+    public class ApacheKafkaConsumer
+    {
+        public class ApacheKafkaConsumerService : IHostedService
+        {
+            private readonly string topic = "feedback";
+            private readonly string groupId = "feedback_group";
+            private readonly string bootstrapServers = "localhost:9092";
+
+            public Task StartAsync(CancellationToken cancellationToken)
+            {
+                var config = new ConsumerConfig
+                {
+                    GroupId = groupId,
+                    BootstrapServers = bootstrapServers,
+                    AutoOffsetReset = AutoOffsetReset.Earliest
+                };
+
+                try
+                {
+                    using (var consumerBuilder = new ConsumerBuilder
+                    <Ignore, string>(config).Build())
+                    {
+                        consumerBuilder.Subscribe(topic);
+                        var cancelToken = new CancellationTokenSource();
+
+                        try
+                        {
+                            while (true)
+                            {
+                                
+                                var consumer = consumerBuilder.Consume
+                                   (cancelToken.Token);
+                                var test = (consumer.Message.Value);
+                                Debug.WriteLine(test);
+                                //lav DB kald
+                               
+                            }
+                        }
+                        catch (OperationCanceledException)
+                        {
+                            consumerBuilder.Close();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                }
+
+                return Task.CompletedTask;
+            }
+            public Task StopAsync(CancellationToken cancellationToken)
+            {
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
