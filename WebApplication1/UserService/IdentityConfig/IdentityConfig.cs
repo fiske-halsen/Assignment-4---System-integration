ac0f7c6c3ce1f8e1cf838c9adcdff442dd751ba9
Phillip <phillip_andersen@hotmail.com>
2022-11-29 13:18:17 +0100

    D

---
+++ WebApplication1/UserService/IdentityConfig/IdentityConfig.cs
@@ -0,0 +1,34 @@
+﻿using IdentityServer4.Models;
+
+namespace UserService.IdentityConfig
+{
+    public static class IdentityConfig
+    {
+        #region Identity Config
+
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("FeedbackService", "FeedbackService Scope"),
+                new ApiScope("UserService", "UserService Scope"),
+            };
+
+        public static IEnumerable<Client> Clients(IConfiguration configuration)
+        {
+            var feedbackServiceKey = configuration["FeedbackService:Key"];
+            var userServiceKey = configuration["UserService:Key"];
+
+
+            return new List<Client>
+            {
+
+
+            };
+
+
+        }
+
+        #endregion
+    }
+}
---
+++ WebApplication1/UserService/Repository/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Context;
+using UserService.Models;
+
+namespace UserService.Repository
+{
+
+    public interface IUserRepository
+    {
+        public Task<User> GetUserById(int userId);
+        public Task<User> GetUserByEmail(string email);
+        public Task<Role> GetUserRoleById(int userId);
+    }
+
+    public class UserRepository : IUserRepository
+    {
+        private readonly DbApplicationContext _applicationContext;
+
+        public UserRepository(DbApplicationContext dbApplicationContext)
+        {
+            _applicationContext = dbApplicationContext;
+        }
+
+        public async Task<User> GetUserByEmail(string email)
+        {
+            return await _applicationContext.Users.
+                 Include(_ => _.Role)
+                .FirstOrDefaultAsync(_ => _.Email == email);
+        }
+
+        public async Task<User> GetUserById(int userId)
+        {
+            return await _applicationContext.Users.
+                 Include(_ => _.Role)
+                .FirstOrDefaultAsync(_ => _.Id == userId);
+        }
+
+        public async Task<Role> GetUserRoleById(int userId)
+        {
+            var user = await _applicationContext.Users.
+                Include(_ => _.Role)
+               .FirstOrDefaultAsync(_ => _.Id == userId);
+
+            return user.Role;
+        }
+    }
+}
